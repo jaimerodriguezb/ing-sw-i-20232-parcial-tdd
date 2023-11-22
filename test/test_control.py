b191668079cd1ad335f52a49353273516dee8f9a
maicol Ascencio <mascencio63@uan.edu.co>
2023-11-21 21:27:46 -0500

    Red-source

--- test/test_control.py
+++ test/test_control.py
@@ -2,5 +2,15 @@ import unittest
 
 class ControlTalanquera(unittest.TestCase):
 
-    def test_xyz(self):
-        pass
+    def test_succes(self):
+        #fixture
+        placa = "kzd-112"
+        control_talanquera = ControlTalanquera()
+    
+        #Test        
+        revision_placa=control_talanquera.revisar_placas_vehiculo(placa)
+        lifting_barrier=control_talanquera.estado_de_talanquera()
+        
+        #Assertion
+        self.assertTrue(revision_placa and lifting_barrier)
+        
